fc135a852db735dc2eefc2f4ef40d80f5ddb18ea
Max Leiserson <max.leiserson@gmail.com>
2014-04-10 11:35:34 -0400

    added controls to toggle off cancer types in each visualization simultaneously

--- public/js/view.js
+++ public/js/view.js
@@ -21,7 +21,7 @@ var m2 = d3.select(m2Element),
 var elements = [ {name: "mutation_matrix", el: m2Element}, {name: "subnetwork", el: subnetworkElement},
 				 {name: "transcript", el: transcriptElement}, {name: "cnabrowser", el: cnaBrowserElement} ];
 
-// Hard-code the network colors (TODO: more elegant way to do this later)
+// Hard-code the network colors (TODO: more elegant way later)
 var defaultStyle = function(){
 	var sty = { colorSchemes: { network: {} , sampleType: {} } };
 	sty.colorSchemes.network["HPRD"] = "rgb(13, 59, 56)"
@@ -42,6 +42,7 @@ var genes = getParameterByName("genes")
 	datasets = getParameterByName("datasets"),
 	query = "/data/bundle?genes=" + genes + "&datasets=" + datasets;
 
+///////////////////////////////////////////////////////////////////////////
 // Get the data and initialize the view
 d3.json(query, function(err, data){
 	// Create each element's style by merging in the dataset colors and
@@ -55,8 +56,22 @@ d3.json(query, function(err, data){
               style[e.name].colorSchemes.sampleType[name] = data.datasetColors[name];
             });
 		}
-	})
+	});
 
+	///////////////////////////////////////////////////////////////////////////
+	// Parse and store the dataset colors and number of samples, and
+	// by default include all datasets in each visualization
+	var datasetToColor = data.datasetColors,
+		datasetToNumSamples = data.mutation_matrix.typeToNumSamples;
+
+	var datasetData = Object.keys(datasetToColor).map(function(d){
+		return { name: d, color: datasetToColor[d], numSamples: datasetToNumSamples[d], active: true };
+	});
+
+	var datasetToInclude = {};
+	datasetData.forEach(function(d){ datasetToInclude[d.name] = true; });
+
+	///////////////////////////////////////////////////////////////////////////
 	// Add the mutation matrix
 	var m2Chart = mutation_matrix({style: style.mutation_matrix})
 	              .addCoverage()
@@ -72,9 +87,12 @@ d3.json(query, function(err, data){
 	subnet.datum(data.subnetwork_data);
 	subnetChart(subnet);
 
+	///////////////////////////////////////////////////////////////////////////
 	// Add a transcript plot selector, where each transcript is grouped by gene
-	var genes = Object.keys(data.transcript_data);
-	genes.forEach(function(g){
+	var genes = Object.keys(data.transcript_data),
+		firstGene, firstTranscript;
+
+	genes.forEach(function(g, i){
 		var transcripts = Object.keys(data.transcript_data[g]).map(function(t){
 			return { name: t, numMutations: data.transcript_data[g][t].mutations.length };
 		});
@@ -88,6 +106,12 @@ d3.json(query, function(err, data){
 			.append("option")
 			.attr("value", function(d){ return g + "," + d.name; })
 			.text(function(d){ return d.name + " (" + d.numMutations + " mutations)"; });
+
+		// Store the first gene and transcript for initialization later
+		if (i == 0){
+			firstGene = g;
+			firstTranscript = transcripts[0].name;
+		}
 	});
 
 	// Set the default params for the transcript plot
@@ -110,23 +134,21 @@ d3.json(query, function(err, data){
 		transcript.datum(data.transcript_data[geneName][transcriptName]);
 		transcript.append("h5").text(geneName);
 		transcriptChart(transcript);
-	})
-
-	// Add a CNA browser selector to choose the genes
-	var cnaGenes = Object.keys(data.cna_browser_data).map(function(g){
-		return { name: g, numCNAs: data.cna_browser_data[g].segments.length };
+		transcriptChart.filterDatasets(datasetToInclude);
 	});
-	cnaGenes.sort(function(a, b){ return a.numCNAs < b.numCNAs; });
 
-	cnaBrowserSelect.selectAll(".cna-option")
-		.data(cnaGenes).enter()
-		.append("option")
-		.attr("value", function(d){ return d.name; })
-		.text(function(d){ return d.name + " (" + d.numCNAs + " aberrations)"; });
+	// Initialize the transcript plot with the first gene
+	if (genes.length > 0){
+		transcript.datum(data.transcript_data[firstGene][firstTranscript]);
+		transcript.append("h5").text(firstGene);
+		transcriptChart(transcript);
+		transcriptChart.filterDatasets(datasetToInclude);
+	}
 
-	// Watch the CNA browser selector to update the current CNA browser on change
+	///////////////////////////////////////////////////////////////////////////
+	// Add a CNA browser selector to choose the genes
 	var cnaChart = cna_browser({ style: style.cnabrowser });
-	cnaBrowserSelect.on("change", function(){
+	function updateCNAChart(){
 		// Retrieve the current gene
 		var geneName = $(this).val();
 
@@ -136,22 +158,59 @@ d3.json(query, function(err, data){
 		// Update the CNA browser
 		cnaBrowser.datum(data.cna_browser_data[geneName]);
 		cnaChart(cnaBrowser);
-	});
+		cnaChart.filterDatasets(datasetToInclude);
+	}
 
+	// Create the CNA genes data
+	var cnaGenes = Object.keys(data.cna_browser_data).map(function(g){
+		return { name: g, numCNAs: data.cna_browser_data[g].segments.length };
+	});
+	cnaGenes.sort(function(a, b){ return a.numCNAs < b.numCNAs; });
+
+	// Watch the CNA browser selector to update the current CNA browser on change
+	cnaBrowserSelect.selectAll(".cna-option")
+		.data(cnaGenes).enter()
+		.append("option")
+		.attr("id", function(d){ return "cna-option-" + d.name; })
+		.attr("value", function(d){ return d.name; })
+		.attr()
+		.text(function(d){ return d.name + " (" + d.numCNAs + " aberrations)"; })
+	
+	cnaBrowserSelect.on("change", updateCNAChart);
+
+	// Initialize the CNA browser with the first gene
+	if (cnaGenes.length > 0){
+		cnaBrowser.datum(data.cna_browser_data[cnaGenes[0].name]);
+		cnaChart(cnaBrowser);
+		cnaChart.filterDatasets(datasetToInclude);
+	}
+
+	///////////////////////////////////////////////////////////////////////////
 	// Update the control panel
-	var datasetToColor = data.datasetColors,
-		datasetToNumSamples = data.mutation_matrix.typeToNumSamples;
-
-	var datasetData = Object.keys(datasetToColor).map(function(d){
-		return { name: d, color: datasetToColor[d], numSamples: datasetToNumSamples[d] };
-	});
-
 	controls.append("h5").text("Datasets");
 	var datasetEls = controls.append("ul")
 		.attr("id", "datasets")
 		.selectAll(".dataset")
 		.data(datasetData).enter()
-		.append("li");
+		.append("li")
+		.style("cursor", "pointer")
+		.on("click", function(d){
+			// Determine if the dataset is currently active
+			var active = d.active,
+				opacity = active ? 0.5 : 1;
+
+			// Create a dictionary to update the status of the current dataset
+			d.active = !active;
+			datasetToInclude[d.name] = d.active;
+
+			// Filter the mutation matrix, transcript plot, and CNA browser
+			m2Chart.filterDatasets(datasetToInclude);
+			transcriptChart.filterDatasets(datasetToInclude);
+			cnaChart.filterDatasets(datasetToInclude);
+
+			// Fade in/out this dataset
+			d3.select(this).style("opacity", opacity);
+		});
 
 	datasetEls.append("div").attr("class", "dataset-color").style("background", function(d){ return d.color; });
 	datasetEls.append("div").text(function(d){ return d.name + " (" + d.numSamples + ")"; });
