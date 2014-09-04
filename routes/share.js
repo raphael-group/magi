7183bd9fd4e3f3cc1b67100f45d676d90cc29c04
Max Leiserson <max.leiserson@gmail.com>
2014-09-04 16:32:53 -0400

    fix typo in crypto hash algorithm name

--- routes/share.js
+++ routes/share.js
@@ -5,12 +5,11 @@ var mongoose = require( 'mongoose' ),
 
 exports.saveShareURL = function(req, res) {
   var url = req.body.url,
-      hasher = crypto.createHash('RIPEMD-64').update(url),
+      hasher = crypto.createHash('md5').update(url),
       hash = hasher.digest('hex');
 
   var QueryHash = mongoose.model('QueryHash');
 
-  console.log('created', {query: url, queryHash:hash});
   // Store hash if it doesn't exist
   QueryHash.find({query: url, queryHash:hash}, function(err, entries) {
     if (err) {
