25d30d799fdd14e4d752579387854709ed165e30
connor <connor@cs.brown.edu>
2014-09-17 10:57:47 -0400

    Fix lingering mongoose.model() references

--- routes/datasets.js
+++ routes/datasets.js
@@ -46,7 +46,7 @@ exports.view = function view(req, res){
 	var dbID = req.params.datasetID || "";
 
 	// Retrieve the dataset
-	var MongoDataset = mongoose.model( 'Dataset' );
+	var MongoDataset = db.magi.model( 'Dataset' );
 	MongoDataset.findById(dbID, function(err, db){
 		// Throw error (if necessary)
 		if (err){
