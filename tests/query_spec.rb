222c8cae02703aa15bf224b0ab71abab4cad6056
John Shen <shenjohn@cs.brown.edu>
2015-04-23 16:42:41 -0400

    adding todos in README

--- tests/query_spec.rb
+++ tests/query_spec.rb
@@ -11,48 +11,49 @@ test_pass = "Adenine=Uracil"
 site = Site.new(Watir::Browser.new :chrome)
 RSpec.configure do |config|
   config.before(:each) {@home_page = site.home_page.open}
-  config.after(:suite) {site.close}
+#  config.after(:suite) {site.close}
 end
 
 # to run tests, 
 # This tests whether the standard datasets are loaded
 RSpec.describe "MAGI" do
-  it "can query datasets OV and UCEC" do
+  # it "can query datasets OV and UCEC" do
+  #   @home_page.nav_home
+  #   datasets = ["OV", "UCEC"]
+  #   query_page = @home_page.query_gene_sets(datasets)
+  #   datasets.each do |set|
+  #     expect(query_page.verify_dataset(set)).to be true
+  #   end
+  # end
+
+  # it "can query for sample TCGA-CU-A0YR" do
+  #   @home_page.nav_home
+
+  #   sample = "TCGA-CU-A0YR"
+  #   query_page = @home_page.query_sample(sample)
+  #   expect(query_page.verify_sample(sample)).to be true
+  # end
+
+  it "can calculate enrichments for a standard query" do
     @home_page.nav_home
-    datasets = ["OV", "UCEC"]
-    query_page = @home_page.query_gene_sets(datasets)
-    datasets.each do |set|
+
+    std = 'SWI-SNF / TCGA Pan-Cancer'
+    query_page = @home_page.standard_query(std)
+    pancancer_datasets.each do |set|
       expect(query_page.verify_dataset(set)).to be true
     end
-  end
-
-  it "can query for sample TCGA-CU-A0YR" do
-    @home_page.nav_home
-
-    sample = "TCGA-CU-A0YR"
-    query_page = @home_page.query_sample(sample)
-    expect(query_page.verify_sample(sample)).to be true
-  end
-
- #  it "can calculate enrichments for a standard query" do
- #    @home_page.nav_home
-
- #    std = 'SWI-SNF / TCGA Pan-Cancer'
- #    query_page = @home_page.standard_query(std)
- #    pancancer_datasets.each do |set|
- #      expect(query_page.verify_dataset(set)).to be true
- #    end
     
- #    enrichment_page = query_page.nav_to_enrichments
+    enrichment_page = query_page.nav_to_enrichments
 
- # # stale element here - has problems
- #    expect(enrichment_page.category_available?("Gender")).to be true
- #    enrichment_page.select_category("Gender")
+    # stale element here - has problems
+    expect(enrichment_page.gene_available?("ARID2")).to be true
+    enrichment_page.select_gene("ARID2")
 
- #    expect(enrichment_page.gene_available?("ARID2")).to be true
- #    enrichment_page.select_gene("ARID2")
+    expect(enrichment_page.category_available?("Gender")).to be true
+    enrichment_page.select_category("Gender")
 
- #    expect(enrichment_page.contingency_table).exists?.to be true
- #  end
+
+    expect(enrichment_page.contingency_table).exists?.to be true
+  end
 
 end
--- tests/upload_spec.rb
+++ tests/upload_spec.rb
@@ -8,7 +8,9 @@ standard_datasets = ["STAD", "BLCA", "BRCA", "COADREAD", "GBM", "HNSC", "KIRC", 
 test_user = "cbio.tester"
 test_pass = "Adenine=Uracil"
 
+# initialize the browser and give the browser type here
 site = Site.new(Watir::Browser.new :chrome)
+
 RSpec.configure do |config|
   config.before(:each) {@home_page = site.home_page.open}
  # config.after(:suite) {site.close}
@@ -23,13 +25,13 @@ RSpec.describe "MAGI" do
     expect(dataset_page.get_dataset_names).to include(*standard_datasets)
   end
 
-  it "is able to upload datasets" do
+  it "is able to upload dataset" do
     login_page = @home_page.nav_to_login_page
     login_page.login(test_user, test_pass)
 
     upload_page = @home_page.nav_to_upload_page
 
-    # todo: make this path relative to the source code
+    # upload this manifest file to the page
     manifest_file = Pathname.new(Dir.pwd + "/../public/data/manifests/tcga-pancancer-blca.json").cleanpath
     test_name = "BLCA-test"
     home = upload_page.upload_via_manifest(manifest_file, test_name)
