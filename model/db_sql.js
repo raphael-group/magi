a8092d6e4dee6acf94d95e34d262774ab9e5d26a
John Shen <john_shen@brown.edu>
2015-05-11 22:32:10 +0000

    using sql package to build queries

--- model/db_sql.js
+++ model/db_sql.js
@@ -1,18 +1,24 @@
 var pg = require("pg");
-
 var conString = 'postgres://postgres@' + process.env.POSTGRES_PORT_5432_TCP_ADDR + ':5432/';
 
-exports.sql_query = function sql_query(text, values, cb){
-    // gets a client from the client pool                                                                                                                    
+exports.execute = execute
+exports.sql_query = sql_query
+
+function execute(query, cb){
+	q = query.toQuery()
+	sql_query(q.text, q.values, cb)
+}
+
+function sql_query(text, values, cb){
+    // gets a client from the client pool                  
+    console.log("text = ", text, ", values = ", values)
     pg.connect(conString, function(err, client, done) {
         if(err) {
             return console.error('error fetching client from pool', err);
         }
-
         query = client.query(text, values, function(err, result) {
             done(); // releases the client back to the pool                                                                                                  
             cb(err, result);
         });
     })
 }
-
