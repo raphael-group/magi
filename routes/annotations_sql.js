f93efa74f5fe095ec7ae1fc6e312897cf62e72b5
John Shen <john_shen@brown.edu>
2015-05-12 00:07:49 +0000

    moving annotation route to annotation_sql with minimal gene/mutation_class insert

--- routes/annotations_sql.js
+++ routes/annotations_sql.js
@@ -34,7 +34,41 @@ exports.gene = function gene(req, res){
 }
 
 exports.addAnnotation = function gene(req, res) {
-	console.log('/annotations/add_gene')
+	console.log('/annotations/gene_sql/add')
+	console.log('req:', req)
+	console.log('req.body:', req.body)
 	
+	console.log("proxy for: /save/annotation/mutation")
+
+	// Load the posted form
+	var form = new formidable.IncomingForm({});
+
+	// We ignore this if the user isn't logged in
+	if (req.user && req.body){
+		// Add the annotation
+		var query = {
+				gene: req.body.gene,
+				cancer: req.body.cancer,
+				mutation_class: req.body.mutationClass,
+				mutation_type: req.body.mutationType,
+				change: req.body.change,
+				domain: req.body.domain,
+				pmid: req.body.pmid,
+				comment: req.body.comment,
+				user_id: req.user._id,
+				source: "Community"
+			};
+
+		annotations.upsert(query, function(err, annotation){
+			if (err){
+				res.send({ error: "Annotation could not be parsed. " + err });
+				throw new Error(err);
+			}
+			res.send({ status: "Annotation saved successfully!", annotation: { _id: annotation._id } });
+		});
+	}
+	else{
+		res.send({ error: "You must be logged in to annotate." });
+	}
 	// 
 }
