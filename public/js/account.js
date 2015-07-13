c53e8a43b8977e0745142f892a69ec3c22e902af
John Shen <john_shen@brown.edu>
2015-07-13 01:34:35 -0400

    status for removing mutation annotations appears in row that annotation was displayed in on account page

--- public/js/account.js
+++ public/js/account.js
@@ -5,7 +5,41 @@ $().ready(function() {
       $('#consentLog').attr('disabled', false);
       $('#consentLog').attr('checked', res == 'true' ? true : false);
     });
+
+    var successClasses  = 'remove-annotation remove-success',
+    warningClasses = 'remove-annotation remove-warning';
+
+    var successClasses  = 'alert alert-success',
+    warningClasses = 'alert alert-warning';
+
+    function deleteMutation() {
+	uid = $(this).data("uid")
+	parentRow = $(this).parents(".anno-tr")
+	$.ajax({
+	    url: "/delete/annotations/mutation/" + uid,
+	    type: 'GET',
+	    error: function(xhr) {
+		statusOnDelete($(parentRow), 'Database error: ' + xhr.status, warningClasses);
+	    },
+
+	    success: function(response) {
+		if(response.error) {
+		    statusOnDelete($(parentRow), 'Oops, something bad happened: ' + xhr.status, warningClasses)
+		    return;
+		}
+		statusOnDelete($(parentRow), response.status, successClasses);	
+	    }
+	}) 
+    }
+    
+    function statusOnDelete(elem, result, classes) {
+	$(elem).html(result);
+	$(elem).attr('class', classes);
+    }
+    $(".trash-anno-icon").click(deleteMutation);
+
 });
+
 
 $('#consentLog').on('change', function() {
   var checked = this.checked;
