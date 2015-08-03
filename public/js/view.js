901822d1fe8c8cd1fd8ea2e383aa0cc5f1a9adf4
John Shen <john_shen@brown.edu>
2015-08-03 01:15:40 -0400

    restoring behavior where  voting is not displayed for reference-less ppis

--- public/js/view.js
+++ public/js/view.js
@@ -380,8 +380,9 @@ function view(){
 				{type: 'text', text: 'Votes'}
 			].map(gd3.tooltip.datum)
 		];
-		d.categories.forEach(function(n){
-			if (d.references[n].length > 0){
+		
+	    d.categories.forEach(function(n){
+		if (d.references[n].length > 0){
 				d.references[n].forEach(function(ref, i){
 					ref.score = ref.upvotes.length - ref.downvotes.length;
 					if (ref.upvotes.indexOf(user_id) > -1){
@@ -438,7 +439,7 @@ function view(){
 						 }}
 					].map(gd3.tooltip.datum));
 				})
-			} else{
+			} else {
 				refTable.push(gd3.tooltip.datum({type: 'text', text: n}));
 			}
 		});
