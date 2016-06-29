973906f216efecdad0665eb085fb4ee3d8e67fea
Max Leiserson <max.leiserson@gmail.com>
2016-06-29 09:40:41 -0400

    fix datasets now that we use DatasetGroups

--- routes/datasets.js
+++ routes/datasets.js
@@ -12,39 +12,34 @@ exports.manifests = function manifests(req, res){
 // Renders list of all datasets
 exports.index = function index(req, res){
 	console.log('/datasets/index')
-	Dataset.datasetGroups({is_public: true}, function(err, publicGroups){
+	// Load all public datasets plus the user's datasets
+	if (req.user){
+		var query = {$or: [{is_public: true}, {user_id: req.user._id }]}
+	} else{
+		var query = {is_public: true};
+	}
+
+	Dataset.datasetGroups(query, function(err, groups){
 		// Throw error (if necessary)
 		if (err) throw new Error(err);
 
-		// Append the groupClass public to each group
-		publicGroups.forEach(function(g){
-			g.dbs = g.dbs.sort(function(a, b){ return a.title > b.title ? 1 : -1; });
+		// Sort the datasets in each group
+		groups.forEach(function(g){
+			g.datasets = g.datasets.sort(function(a, b){ return a.title > b.title ? 1 : -1; });
 		});
 
+		// Append the groupClass public to each group
+		// Store the checkbox IDs of all and the public datasets by group
+		var userGroups = req.user ? groups.filter(function(g){ return (g.user_id + "") == (req.user._id + ""); }) : [],
+			publicGroups = groups.filter(function(g){ return g.is_public; });
 
 		var groupData = [{groups: publicGroups, ty: "public"}];
 
-		// Load the user's datasets (if necessary)
-		if (req.user){
-			Dataset.datasetGroups({user_id: req.user._id}, function(err, userGroups){
-				// Throw error (if necessary)
-				if (err) throw new Error(err);
-
-				// Append the groupClass standard to each group
-				userGroups.forEach(function(g){
-					g.dbs = g.dbs.sort(function(a, b){ return a.title > b.title ? 1 : -1; });
-				});
-
-				if (userGroups.length > 0){
-					groupData = [{groups: userGroups, ty: "user"}, groupData[0]];
-				}
-
-				res.render('datasets/index', { user: req.user, groupClasses: groupData });
-			});
+		if (userGroups.length > 0){
+			groupData.push({groups: userGroups, ty: "user"});
 		}
-		else{
-			res.render('datasets/index', { user: req.user, groupClasses: groupData });
-		}
+
+		res.render('datasets/index', { user: req.user, groupClasses: groupData });
 	});
 }
 
@@ -53,7 +48,8 @@ exports.view = function view(req, res){
 	var dbID = req.params.datasetID || "";
 
 	// Retrieve the dataset
-	var MongoDataset = Database.magi.model( 'Dataset' );
+	var MongoDataset = Database.magi.model( 'Dataset' ),
+		MongoGroup   = Database.magi.model('DatasetGroup');
 	MongoDataset.findById(dbID, function(err, db){
 		// Throw error (if necessary)
 		if (!db || err){
@@ -64,11 +60,17 @@ exports.view = function view(req, res){
 		// Check if the dataset is standard, and render it if it is
 		// or if the user owns the database (add the "" to make sure)
 		// the id is a string and not an ObjectId
-		if (db.is_public || (db.user_id + "") == (req.user._id + "")){
-			res.render('datasets/view', { user: req.user, db: db });
-		}
-		else{
+		if (!db.is_public && (!req.user || (db.user_id + "") != (req.user._id + ""))){
 			res.redirect('/datasets');
+		} else {
+			if (db.group_id){
+				MongoGroup.findById(db.group_id, function(err, group){
+					if (err) console.err(err);
+					res.render('datasets/view', { user: req.user, db: db, group: group });
+				});
+			} else{
+				res.render('datasets/view', { user: req.user, db: db, group: {} });
+			}
 		}
 	});
 }
